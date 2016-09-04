d146902f77882a0a1452157ea27615d7b567d8a4
Manoj Shevate <manoj.shevate@gmail.com>
2016-09-04 22:30:58 +0530

    First checking - Hello Aureliad basic app

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Aurelia
 Hello Aurelia
+
+
+#Steps
+1. npm install -g jspm gulp express
+2. jspm install
+3. node server.js
+4. App will running on http://localhost:2112/
---
+++ src/app.js
@@ -0,0 +1,8 @@
+export class App {
+	constructor(){
+		this.message = "Hello Aurelia!!"
+		this.image = "https://avatars3.githubusercontent.com/u/5877762"
+		this.input = "";
+	}
+}
+
